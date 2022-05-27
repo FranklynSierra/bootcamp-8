c8345fe1174727317873f8969d6bcce3f8933454
fran <franklynssierracontreras@gmail.com>
2022-05-27 12:25:28 -0400

    proyecto 8 de OpenBootcamp

---
+++ numero.js
@@ -0,0 +1,64 @@
+
+//numeros 
+
+let centimetro=10;
+
+let metros=5.1;
+
+let kilogramos=6.7;
+
+let metro_redondeo=parseInt(metros)
+let kilo_redondeo=Math.round(kilogramos)
+
+
+let maximo=Number.MAX_VALUE +1;
+
+let otro_maximo=2**1023+1;
+
+console.log(metro_redondeo)
+
+console.log(kilo_redondeo)
+
+
+console.log(metros.valueOf())
+
+console.log(maximo)
+
+
+console.log(otro_maximo)
+
+
+
+
+// let n='5.595'
+// let n2=172.23
+
+
+// console.log(typeof n)
+// console.log(n+n2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
